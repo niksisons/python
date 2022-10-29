ee58a595ce7d25885ba1de49ad1ee0ed1fcd0103
niksisons <Rogozhin.nickita2018@yandex.ru>
2022-10-29 20:33:51 +0300

    new

---
+++ task/module/2.py
@@ -0,0 +1,2 @@
+t, h = int(input()), int(input())
+print((t * h) / 2)
---
+++ task/module/7.py
@@ -0,0 +1,2 @@
+n = input().split()
+print(abs((int(n[0]) * 3600 + int(n[1]) * 60 + int(n[2])) - (int(n[3]) * 3600 + int(n[4]) * 60 + int(n[5]))))
