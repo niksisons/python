2b521502fbf8372b93022cbcf502591ea8a6054d
niksisons <rogozhin.nickita2018@yandex.ru>
2022-11-01 14:47:03 +0300

    mda

--- practic work/paket/par.py
+++ practic work/paket/par.py
@@ -1,11 +1,12 @@
 import random
 
-def p():
+
+def p(n):
     rez1 = 0
     rez2 = 0
     rez3 = 0
     rez4 = 0
-    for i in range(10001):
+    for i in range(n):
         three_door = [0, 0, 1]
         player = random.choice(three_door)
         two_door = three_door[1:]
@@ -21,4 +22,4 @@ def p():
     return (rez1 / 100), (rez2 / 100), (rez3 / 100), (rez4 / 100), f'Ответ: {((rez1 + rez2 + rez3) / 100)}'
 
 
-print(p())
+print(p(10001))
