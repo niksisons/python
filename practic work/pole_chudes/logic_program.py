6080689461c5d0e90cfcc591e64ebe767f4d8e78
niksisons <Rogozhin.nickita2018@yandex.ru>
2022-11-06 16:01:38 +0300

    new

--- practic work/pole_chudes/logic_program.py
+++ practic work/pole_chudes/logic_program.py
@@ -2,12 +2,12 @@ import random
 
 while True:
     with open(r'C:\Users\nikita\PycharmProjects\python\practic work\pole_chudes\slova.txt', encoding='UTF8') as sl:
-        sl1 = random.choice(sl.read().splitlines())
+        sl1 = random.choice(sl.read().splitlines()).lower()
         black_box = ['\u25A0' for i in range(len(sl1))]
-    with open(r'C:\Users\nikita\PycharmProjects\python\practic work\pole_chudes\records.txt', mode='a+',
+    with open(r'C:\Users\nikita\PycharmProjects\python\practic work\pole_chudes\records.txt', mode='r+',
               encoding='UTF8') as r:
         rec = r.read()
-        print(f'Ваш прошлый рекорд: {rec}')
+        print(f'Ваш текущий рекорд: {rec}')
         complexity = int(input('Выберите сложность(1, 2, 3, 4-Выйти): '))
         if complexity == 1:
             lives = 7
@@ -18,16 +18,11 @@ while True:
         else:
             break
         count = 0
-        while count != len(sl1):
-            if lives == 0:
-                break
+        while black_box.count('\u25A0') and lives:
             n = input(f'Назовите букву: {"".join(black_box)}\n')
             if len(n) == 1:
                 if n in sl1.lower():
-                    ind = sl1.lower().index(n)
-                    if black_box[ind] == n:
-                        continue
-                    black_box[ind] = n
+                    black_box = [n if n == sl1[i] else black_box[i] for i in range(len(sl1))]
                     count += 1
                 else:
                     print('Такой буквы нет')
