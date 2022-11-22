102c10df347f8becba1ec47ecdd142edc1fb6484
niksisons <rogozhin.nickita2018@yandex.ru>
2022-11-22 16:09:07 +0300

    update mistake

--- practicwork/mistake/find_mistake.py
+++ practicwork/mistake/find_mistake.py
@@ -8,44 +8,44 @@ def opn_f(n):
         print(num_list[i])
     f.close()
 
-opn_f('opn')
-# try:
-#     name = input('Введите имя файла: ')
-#     print(opn_f(name))
-# except FileNotFoundError:
-#     print("Такого файла нет!")
-#     rez = False
-# except NameError:
-#     print("Такого имени нет!")
-#     rez = False
-# except ValueError:
-#     print("Превая сторока имеет недопустимое значение!")
-#     rez = False
-# except TypeError:
-#     print("Несовместимые аргументы!")
-#     rez = False
-# except ZeroDivisionError:
-#     print('Неверные данные для итерации!!')
-#     rez = False
-# except MemoryError:
-#     print("Недостаточно оперативной памяти!")
-#     rez = False
-# except StopIteration:
-#     print("Неверные данные для итерации!*")
-#     rez = False
-# except IndexError:
-#     print("Неверные данные для итерации!/")
-#     rez = False
-# except:
-#     print("Fatality error!")
-#     rez = False
-# else:
-#     print(opn_f(name))
-#     rez = True
-# finally:
-#     try:
-#         print(f"Try: {'Успешно' if rez else 'Провал'}")
-#     except NameError:
-#         print("Переменная не найдена")
-#     except:
-#         print("Fatality error!")
+
+try:
+    name = input('Введите имя файла: ')
+    print(opn_f(name))
+except FileNotFoundError:
+    print("Такого файла нет!")
+    rez = False
+except NameError:
+    print("Такого имени нет!")
+    rez = False
+except ValueError:
+    print("Превая сторока имеет недопустимое значение!")
+    rez = False
+except TypeError:
+    print("Несовместимые аргументы!")
+    rez = False
+except ZeroDivisionError:
+    print('Неверные данные для итерации!!')
+    rez = False
+except MemoryError:
+    print("Недостаточно оперативной памяти!")
+    rez = False
+except StopIteration:
+    print("Неверные данные для итерации!*")
+    rez = False
+except IndexError:
+    print("Неверные данные для итерации!/")
+    rez = False
+except:
+    print("Fatality error!")
+    rez = False
+else:
+    print(opn_f(name))
+    rez = True
+finally:
+    try:
+        print(f"Try: {'Успешно' if rez else 'Провал'}")
+    except NameError:
+        print("Переменная не найдена")
+    except:
+        print("Fatality error!")
